46af2928f04c50919241d0e576c1043392908be6
Nodirbek Sharipov <mail@nodir.dev>
2020-05-07 19:01:45 +0500

    New update

---
+++ precache-manifest.abc79fd770e34ab478f2e81a65cccdb6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a677d758b98a90b7bbb3b605b65469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83aee818ee7d940632b6",
+    "url": "/static/css/main.9ddc6c37.chunk.css"
+  },
+  {
+    "revision": "2a819c63f7ef508b5cdd",
+    "url": "/static/js/2.184295bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.184295bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83aee818ee7d940632b6",
+    "url": "/static/js/main.076747d1.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "243b6ef9de8d326b56c2353186269b59",
+    "url": "/static/media/SpaceGrotesk-Bold.243b6ef9.woff2"
+  },
+  {
+    "revision": "7a815d800b552622301bf8186a48b9e2",
+    "url": "/static/media/SpaceGrotesk-Bold.7a815d80.woff"
+  },
+  {
+    "revision": "9ff0e90dfa6478d61e54183b8a7342af",
+    "url": "/static/media/SpaceGrotesk-Regular.9ff0e90d.woff2"
+  },
+  {
+    "revision": "b05e4e837d23ed2ad1bc648857252d2b",
+    "url": "/static/media/SpaceGrotesk-Regular.b05e4e83.woff"
+  },
+  {
+    "revision": "f2c1124c3967c2c5efa74de339fb801a",
+    "url": "/static/media/bg.f2c1124c.jpg"
+  },
+  {
+    "revision": "6a69b4ac24e6b110f018c221fbfdcec3",
+    "url": "/static/media/fg.6a69b4ac.png"
+  },
+  {
+    "revision": "27e941117d782a5a6b3010aae9d6632b",
+    "url": "/static/media/github.27e94111.svg"
+  },
+  {
+    "revision": "cf53740fd3465bad6c02ded76f6eb5e5",
+    "url": "/static/media/instagram.cf53740f.svg"
+  },
+  {
+    "revision": "475aa12277988507c9ad8f1d179f8b35",
+    "url": "/static/media/mail.475aa122.svg"
+  },
+  {
+    "revision": "66c70af9dedab4651b20cacf328e95bc",
+    "url": "/static/media/phone.66c70af9.svg"
+  },
+  {
+    "revision": "3a0aa3e69cc5982fa45a42bf0470bff6",
+    "url": "/static/media/telegram.3a0aa3e6.svg"
+  },
+  {
+    "revision": "46015ea74fe0ef6d3f261bfbb62f9838",
+    "url": "/static/media/twitter.46015ea7.svg"
+  }
+]);
